1406d67597300c1f3c358aff523c32014e6610c1
juani <juangarnero@hotmail.com.ar>
2022-09-20 17:24:25 -0300

    avance en el restrucutado de table

--- src/pages/GeneralDisplay/Section/Section.js
+++ src/pages/GeneralDisplay/Section/Section.js
@@ -1,66 +1,60 @@
 import "./Section.css";
 
-import Tabla from '../Tabla/Tabla';
+import Table from '../Table/Table';
+import Dashboard from "../Dashboard&Buttons/Dashboard";
 
 const products = [
-    {
-      id:1,
-      nombre:"HONDA cbr 1000 ",
-      cantidad: 2,
-      fi: "13/09/2022",
-      fv:0,
-      price: 42500
-    },
-    {
-      id:2,
-      nombre:"BMW s1000rr ",
-      cantidad: 3,
-      fi: "13/09/2022",
-      fv:0,
-      price: 55000
-    },
-    {
-      id:3,
-      nombre:"Kawasaki H2r",
-      cantidad:1,
-      fi:"13/09/2022",
-      fv:0,
-      price: 90000
-    },
-    {
-      id:3,
-      nombre:"Yamaha r1 ",
-      cantidad:5,
-      fi:"13/09/2022",
-      fv:0,
-      price: 57900
-    }
-  ]
+  {
+    id:1,
+    nombre:"HONDA cbr 1000 ",
+    cantidad: 2,
+    fi: "13/09/2022",
+    fv:0,
+    price: 42500
+  },
+  {
+    id:2,
+    nombre:"BMW s1000rr ",
+    cantidad: 3,
+    fi: "13/09/2022",
+    fv:0,
+    price: 55000
+  },
+  {
+    id:3,
+    nombre:"Kawasaki H2r",
+    cantidad:1,
+    fi:"13/09/2022",
+    fv:0,
+    price: 90000
+  },
+  {
+    id:4,
+    nombre:"Yamaha r1 ",
+    cantidad:5,
+    fi:"13/09/2022",
+    fv:0,
+    price: 57900
+  },
+  {
+    id:5,
+    nombre:"Suzuki gsx1000r",
+    cantidad:6,
+    fi:"20/09/2022",
+    fv:0,
+    price: 61300
+  }
+]
 
 
 
 const Section = () => {
     
     return (
-        <>
-            <div className='container'>
-                <table>
-                <tbody>
-                    <tr>
-                    {products.map((item) => <Tabla 
-                    key={item.id}
-                    nombre={item.nombre}
-                    cantidad={item.cantidad}
-                    fi={item.fi} 
-                    fv={item.fv} 
-                    price={item.price}
-                    /> ) 
-                    }
-                    </tr>
-                </tbody>
-                </table>
-            </div>
-        </>
+      <>
+        <Dashboard/>
+        <Table products={products}/>
+      </>
     )
 }
 
