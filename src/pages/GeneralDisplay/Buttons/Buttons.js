eba0930887ed480f0266ee00dfdaef237571f90b
juani <juangarnero@hotmail.com.ar>
2022-09-26 20:22:52 -0300

    avance axuliar

--- src/pages/GeneralDisplay/Buttons/Buttons.js
+++ src/pages/GeneralDisplay/Buttons/Buttons.js
@@ -4,6 +4,9 @@ const Buttons = () => {
   return (
     <div className='buttons-section'>
         <div className="Buttons-styles">
+            {/* ventas && <button>vender</button>*/}
+            {/* elimnar && <button>eliminar</button>*/}
+            {/* cargar && <button>cargar</button>*/}
             <button>vender</button>
             <button>eliminar</button>
             <button>cargar</button>
