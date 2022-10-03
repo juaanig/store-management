68370bc90325abb1244ec74eda09851a74dc1b0a
LucasButto <lbutto40@gmail.com>
2022-10-03 19:29:38 -0300

    mapeado de botones segun rol

--- src/pages/GeneralDisplay/Buttons/Buttons.js
+++ src/pages/GeneralDisplay/Buttons/Buttons.js
@@ -1,12 +1,25 @@
 import "./Buttons.css"
 
 const Buttons = () => {
+
+  const role = "vendedor";
+
+  const roleButtonHandler = (role) => {
+    switch (role) {
+      case "vendedor":
+        return ( <button className="common">Vender</button> );
+      case "comprador":
+        return ( <button className="common">Comprar</button> );
+      case "deposito":
+        return (<button className="common">Cargar</button>);
+      default:
+        return (<h2>Que haces acá master?</h2>);
+    }
+  }
   return (
     <div className='buttons-section'>
         <div className="buttons-styles">
-            <button className="common sell">Vender</button>
-            <button className="common buy">Comprar</button>
-            <button className="common add">Cargar</button>
+          {roleButtonHandler(role)}
         </div>
     </div>
   )
