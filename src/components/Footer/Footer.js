8f8262b368fa3eff84d96dd9fc389cf9d5bfab2b
LucasButto <lbutto40@gmail.com>
2022-10-18 00:19:03 -0300

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import {Card, Container} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <Card.Footer className="mt-5 bg-dark text-center text-white" id="footer">
+    <Card.Footer className="bg-dark text-center text-white" id="footer">
         <Container className="p-4 pb-0">
             <section className="mb-4">
             
-                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i className="fab fa-github"></i>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/juaanig" role="button" target={"_blank"} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    Juani
                 </a>
 
-                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i className="fab fa-github"></i>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/LucasButto" role="button" target={"_blank"} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    Lucas
                 </a>
                 
-                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i className="fab fa-github"></i>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/franmux01" role="button" target={"_blank"} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    Fran
                 </a>
             </section>
         </Container>
