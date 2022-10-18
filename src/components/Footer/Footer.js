4345c8dcf6d471430152214c45de87bdecd9b7da
LucasButto <lbutto40@gmail.com>
2022-10-18 00:24:16 -0300

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,20 +3,23 @@ import {Card, Container} from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <Card.Footer className="mt-5 bg-dark text-center text-white" id="footer">
+    <Card.Footer className="bg-dark text-center text-white" id="footer">
         <Container className="p-4 pb-0">
             <section className="mb-4">
             
-                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i className="fab fa-github"></i>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/juaanig" role="button" target={"_blank"} rel="noreferrer">
+                    <i className="fab fa-github"></i>
+                    Juani
                 </a>
 
-                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i className="fab fa-github"></i>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/LucasButto" role="button" target={"_blank"} rel="noreferrer">
+                    <i className="fab fa-github"></i>
+                    Lucas
                 </a>
                 
-                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                    ><i className="fab fa-github"></i>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/franmux01" role="button" target={"_blank"} rel="noreferrer">
+                    <i className="fab fa-github"></i>
+                    Fran
                 </a>
             </section>
         </Container>
