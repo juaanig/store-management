d3d0ed0184b6ce317063e6b97893b87ae590ec81
juani <juangarnero@hotmail.com.ar>
2022-10-23 21:47:45 -0300

    problem fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,36 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
+  return (
+    <Card.Footer className="bg-dark text-center text-white" id="footer">
+        <Container className="p-4 pb-0">
+            <section className="mb-4">
 
-  return(
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/juaanig" role="button" target={"_blank"} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    Juani
+                </a>
 
-    <>
-        <Card.Footer className="bg-dark text-center text-white" id="footer">
-            <Container className="p-4 pb-0">
-                <section className="mb-4">
-                
-                    <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/juaanig" role="button" target={"_blank"} rel="noreferrer">
-                        <FontAwesomeIcon icon={faGithub} />
-                        Juani
-                    </a>
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/LucasButto" role="button" target={"_blank"} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    Lucas
+                </a>
 
-                    <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/LucasButto" role="button" target={"_blank"} rel="noreferrer">
-                        <FontAwesomeIcon icon={faGithub} />
-                        Lucas
-                    </a>
-                    <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/franmux01" role="button" target={"_blank"} rel="noreferrer">
-                        <FontAwesomeIcon icon={faGithub} />
-                        Fran
-                    </a>
-                </section>
-            </Container>
-            <div className="text-center p-3 bg-dark">
-                © 2022 Copyright:
-                <a className="text-white" href="#!"> Mulé Garnero Butto & Co.</a>
-            </div>
-        </Card.Footer>
-    </>
-    );
+                <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/franmux01" role="button" target={"_blank"} rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    Fran
+                </a>
+            </section>
+        </Container>
+        <div className="text-center p-3 bg-dark">
+            © 2022 Copyright:
+            <a className="text-info" href="https://github.com/juaanig/store-management" target={"_blank"} rel="noreferrer"> Mulé Garnero Butto & Co.</a>
+        </div>
+    </Card.Footer>
+  );
 }
 
 export default Footer;
