94164b482a0e3e5bb355c56a2ca03e01c349c5b1
juani <juangarnero@hotmail.com.ar>
2022-11-03 16:07:50 -0300

    test deploy 0012

--- src/components/Nav/NavLayout.js
+++ src/components/Nav/NavLayout.js
@@ -6,12 +6,9 @@ import LogoutButton from "../Buttons/LogOut";
 import AuthContext from "../../contexts/authContext/AuthContext";
 import ThemeButton from "../Buttons/ThemeButton";
 
-import Dropdown from "react-bootstrap/Dropdown";
-import ThemeContext from "../../contexts/ThemeContext/ThemeContext";
 
 const NavLayout = () => {
   const { user } = useContext(AuthContext);
-  const {theme} =useContext(ThemeContext)
 
   return (
     <>
@@ -20,27 +17,10 @@ const NavLayout = () => {
           STORE MANAGMENT
         </Navbar.Brand>
         <Nav className="ms-auto">
-
-          <Dropdown>
-
-            <Dropdown.Toggle className="me-5" variant="success" id="dropdown-basic">
-              OPCIONES
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu  variant={theme}>
-
-              <Dropdown.Item key="action-1">
-                <ThemeButton />
-              </Dropdown.Item>
-
-              <Dropdown.Item key="action-2">
-                {user && <LogoutButton />}
-              </Dropdown.Item>
-
-            </Dropdown.Menu>
-          </Dropdown>
-          
-          
+          <div>
+            <ThemeButton />
+            {user && <LogoutButton />}
+          </div>
         </Nav>
       </Navbar>
     </>
