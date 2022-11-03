ba0c732cd336f55934d590f3ce1d8de46113a571
juani <juangarnero@hotmail.com.ar>
2022-11-03 16:20:04 -0300

    something error

--- src/components/Nav/NavLayout.js
+++ src/components/Nav/NavLayout.js
@@ -6,9 +6,12 @@ import LogoutButton from "../Buttons/LogOut";
 import AuthContext from "../../contexts/authContext/AuthContext";
 import ThemeButton from "../Buttons/ThemeButton";
 
+import Dropdown from "react-bootstrap/Dropdown";
+import ThemeContext from "../../contexts/ThemeContext/ThemeContext";
 
 const NavLayout = () => {
   const { user } = useContext(AuthContext);
+  const {theme} =useContext(ThemeContext)
 
   return (
     <>
@@ -17,10 +20,27 @@ const NavLayout = () => {
           STORE MANAGMENT
         </Navbar.Brand>
         <Nav className="ms-auto">
-          <div>
-            <ThemeButton />
-            {/*user && <LogoutButton />*/}
-          </div>
+
+          <Dropdown>
+
+            <Dropdown.Toggle className="me-5" variant="success" id="dropdown-basic">
+              OPCIONES
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu  variant={theme}>
+
+              <Dropdown.Item key="action-1">
+                <ThemeButton />
+              </Dropdown.Item>
+
+              <Dropdown.Item key="action-2">
+                {user && <LogoutButton />}
+              </Dropdown.Item>
+
+            </Dropdown.Menu>
+          </Dropdown>
+          
+          
         </Nav>
       </Navbar>
     </>
